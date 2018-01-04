e03ac52b64c45d4d0adecfe2a06315b1f460bbac
davetoland <git@devfire.net>
2018-01-04 10:14:46 +0000

    Bug fix

--- DependencyInjectionPrototype.Tests/UnityBootstrapper.cs
+++ DependencyInjectionPrototype.Tests/UnityBootstrapper.cs
@@ -7,9 +7,9 @@ namespace DependencyInjectionPrototype.Tests
         public IInjectionContainer GetContainer()
         {
             var container = new UnityInjectionContainer();
-            container.Register<IClass1, Class1>();
-            container.Register<IClass2, Class2>();
-            container.Register<IClass3, Class3>();
+            container.Register<IClass1, MockClass1>();
+            container.Register<IClass2, MockClass2>();
+            container.Register<IClass3, MockClass3>();
 
             return container;
         }
